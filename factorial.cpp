d82201e003effc61cedb45ff5df8dd17f7b57f1f
mradulpaliwal <mradulpaliwal2003@gmail.com>
2023-07-08 16:18:24 +0530

    factorial

---
+++ factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,f=1,v=1;
+cout<<"enter no";
+cin>>n;
+if(n==0)
+{
+    cout<<"factorial is"<<v;
+
+}
+else
+{
+
+for(int i=1;i<=n;i++)
+{
+f=(f*i);
+
+}
+cout<<"factorial is"<<f;
+}
+}
